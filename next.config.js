f8d1b3d8b678a761080018ff5cec5f4198805fa4
Nicola D'Aniello <nicola.danie@gmail.com>
2019-08-19 12:38:29 +0100

    add enviroment variables

--- next.config.js
+++ next.config.js
@@ -5,7 +5,15 @@ const withPurgeCss = require("next-purgecss");
 module.exports = withCSS(
   withSass(
     withPurgeCss({
-      purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer
+      purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer,
+      serverRuntimeConfig: {
+        // Will only be available on the server side
+        // mySecret: "secret"
+      },
+      publicRuntimeConfig: {
+        // Will be available on both server and client
+        graphqlEndpoint: "http://localhost:8888/wordpress/graphql"
+      }
     })
   )
 );
